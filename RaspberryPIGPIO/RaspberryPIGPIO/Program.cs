f53a8f4f765af096b737cf0d0482d424c351265f
MCK9595 <yoshi.macky95@gmail.com>
2019-08-03 00:39:05 +0900

    Pinの電流のOnOffを指定回数繰り返す処理を作成

--- RaspberryPIGPIO/RaspberryPIGPIO/Program.cs
+++ RaspberryPIGPIO/RaspberryPIGPIO/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Device.Gpio;
+using System.Threading;
 
 namespace RaspberryPIGPIO
 {
     class Program
     {
+        // ピン番号
+        private static readonly int pinNum = 7;
+        // 繰り返し回数
+        private static readonly int N = 3;
+        // 光っている時間(ms)
+        private static readonly int lightTime = 300;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{pinNum}を付けたり消したりします。");
+            // 物理のPIN位置を指定
+            GpioController controller = new GpioController(PinNumberingScheme.Board);
+            controller.OpenPin(pinNum, PinMode.Input);
+
+            try
+            {
+                for(var i = 0; i < N; i++)
+                {
+                    // 電気を流す
+                    controller.Write(pinNum, PinValue.High);
+                    Thread.Sleep(lightTime);
+                    // 電気を消す
+                    controller.Write(pinNum, PinValue.Low);
+                    Thread.Sleep(lightTime);
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
         }
     }
 }
