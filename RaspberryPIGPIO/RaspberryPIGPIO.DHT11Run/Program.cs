3666b841f5c16bf1d477339fb753deeab27696c9
MCK9595 <yoshi.macky95@gmail.com>
2019-08-12 03:47:39 +0900

    DHT11の操作を、ラッパーからGPIOクラスに変更

--- RaspberryPIGPIO/RaspberryPIGPIO.DHT11Run/Program.cs
+++ RaspberryPIGPIO/RaspberryPIGPIO.DHT11Run/Program.cs
@@ -1,6 +1,5 @@
-﻿using Iot.Device.DHTxx;
-using Iot.Units;
-using System;
+﻿using System;
+using System.Device.Gpio;
 
 namespace RaspberryPIGPIO.DHT11Run
 {
@@ -8,13 +7,22 @@ namespace RaspberryPIGPIO.DHT11Run
     {
         static void Main(string[] args)
         {
-            using (Dht11 dht = new Dht11(7, System.Device.Gpio.PinNumberingScheme.Board))
-            {
-                Temperature temperature = dht.Temperature;
-                double humidity = dht.Humidity;
+            int pinNum = 7;
+            GpioController controller = new GpioController(PinNumberingScheme.Board);
+            controller.OpenPin(pinNum, PinMode.Input);
 
-                Console.WriteLine($"気温:{temperature.Celsius}度\t湿度:{humidity}パーセント");
-            }
+            var a = controller.Read(pinNum);
+
+            Console.WriteLine($"{a}");
+
+            // 今は使えないので、コメントアウト
+            //using (Dht11 dht = new Dht11(7, System.Device.Gpio.PinNumberingScheme.Board))
+            //{
+            //    Temperature temperature = dht.Temperature;
+            //    double humidity = dht.Humidity;
+
+            //    Console.WriteLine($"気温:{temperature.Celsius}度\t湿度:{humidity}パーセント");
+            //}
         }
     }
 }
