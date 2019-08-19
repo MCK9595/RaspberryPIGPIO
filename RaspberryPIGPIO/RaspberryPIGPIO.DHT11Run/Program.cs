f5a6d3851b5352be412bd0c13f8f18f48b171bdb
MCK9595 <yoshi.macky95@gmail.com>
2019-08-20 02:09:35 +0900

    GPIOのラッパークラスを使う処理に修正

--- RaspberryPIGPIO/RaspberryPIGPIO.DHT11Run/Program.cs
+++ RaspberryPIGPIO/RaspberryPIGPIO.DHT11Run/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Iot.Device.DHTxx;
+using Iot.Units;
+using System;
 using System.Device.Gpio;
 using System.Threading.Tasks;
 
@@ -11,48 +13,17 @@ namespace RaspberryPIGPIO.DHT11Run
             try
             {
                 int pinNum = 7;
-                int loopCount = 100000;
-                GpioController controller = new GpioController(PinNumberingScheme.Board);
-                controller.OpenPin(pinNum);
-                // 対象のPinをHighにする
-                controller.SetPinMode(pinNum, PinMode.Output);
-                controller.Write(pinNum, PinValue.Low);
-                await Task.Delay(20);
-                controller.Write(pinNum, PinValue.High);
-                await Task.Delay(30);
 
-                controller.SetPinMode(pinNum, PinMode.InputPullUp);
-                var count = loopCount;
-
-                // 正常性チェック・・・？
-                while (controller.Read(pinNum) == PinValue.Low)
+                //今は使えないので、コメントアウト
+                using (Dht11 dht = new Dht11(pinNum, System.Device.Gpio.PinNumberingScheme.Board))
                 {
-                    if (count-- == 0)
-                    {
-                        Console.WriteLine($"{loopCount}回Highが続いたので、終了します。");
-                        return;
-                    }
+                    Temperature temperature = dht.Temperature;
+                    double humidity = dht.Humidity;
+
+                    Console.WriteLine($"気温:{temperature.Celsius}度\t湿度:{humidity}パーセント");
                 }
-
-                count = loopCount;
-                while (controller.Read(pinNum) == PinValue.High)
-                {
-                    if (count-- == 0)
-                    {
-                        Console.WriteLine($"{loopCount}回Highが続いたので、終了します。");
-                        return;
-                    }
-                }
-
-                // 今は使えないので、コメントアウト
-                //using (Dht11 dht = new Dht11(7, System.Device.Gpio.PinNumberingScheme.Board))
-                //{
-                //    Temperature temperature = dht.Temperature;
-                //    double humidity = dht.Humidity;
-
-                //    Console.WriteLine($"気温:{temperature.Celsius}度\t湿度:{humidity}パーセント");
-                //}
-            }catch(Exception ex)
+            }
+            catch(Exception ex)
             {
                 Console.WriteLine($"エラー発生:{ex.Message}");
             }
